ab09d7d6ddec7a2133e1aa7a93e3a87f8f8385a1
Abigail Fox <abigailfireflyfox@gmail.com>
2021-10-25 15:45:38 -0400

    adding functionality for quote addition to CoreData

--- abigail-app/widgets/QuoteWidget.swift
+++ abigail-app/widgets/QuoteWidget.swift
@@ -18,8 +18,28 @@ struct QuoteWidget: View {
 
     var body: some View {
         VStack {
+            Button(action: addQuote) {
+                Label("Add Item", systemImage: "plus")
+            }
             Text("Quote")
             Text("Source")
+        }
+    }
+    
+    func addQuote() {
+        print("I am a quote")
+        
+        let newQuote = Quote(context: viewContext)
+        newQuote.source = "source"
+        newQuote.quote = "quote"
+
+        do {
+            try viewContext.save()
+        } catch {
+            // Replace this implementation with code to handle the error appropriately.
+            // fatalError() causes the application to generate a crash log and terminate. You should not use this function in a shipping application, although it may be useful during development.
+            let nsError = error as NSError
+            fatalError("Unresolved error \(nsError), \(nsError.userInfo)")
         }
     }
 }
